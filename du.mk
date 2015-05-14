84810326f71542b76d25ff48a7503aa5b2697612
bhushan2k <bhushan2k@users.noreply.github.com>
2015-05-14 19:10:44 +0530

    Update and rename cm.mk to du.mk

--- du.mk
+++ du.mk
@@ -1,9 +1,9 @@
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := du_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
